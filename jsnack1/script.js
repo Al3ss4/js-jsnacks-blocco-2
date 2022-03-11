2dbbe37f798eb31ac3e699f643fa4cbd9a11ba90
Mario Rovito <alessandro.r14@gmail.com>
2022-03-11 14:06:40 +0100

    add array

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,9 +3,10 @@ console.log('JS OK');
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
 
 
+// creazione array numeri interi
+var arrayInteri = [ 0,1,2,3,4,5,6,7,8,9]
 // crezione let somma 
 let somma = 0;
-
 
 
 
