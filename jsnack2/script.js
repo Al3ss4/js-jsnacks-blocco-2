a2f2921e7d797ee2628a8f4fbb2d47007aca6c44
Mario Rovito <alessandro.r14@gmail.com>
2022-03-11 14:29:08 +0100

    while version

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,25 +6,34 @@ console.log('JS OK');
 // versione for
 
 //creazione del let
-let somma = 0;
+//let somma = 0;
 
 
 //creazione del for 
-for( let i=0; i < 5; i++){
-    let nuovoNumero;
+//for( let i=0; i < 5; i++){
+  //  let nuovoNumero;
     
 
 // prompt con parseInt per dichiarare un numero 
-        nuovoNumero = parseInt(prompt(`${i+1} Inserisci un numero`))
+    //    nuovoNumero = parseInt(prompt(`${i+1} Inserisci un numero`))
     
 
 
 // somma dei numeri inseriti dall'utente
-somma+= nuovoNumero;
+//somma+= nuovoNumero;
         
-}
-
+//}
 
 
 //console log della somma
-console.log('La somma dei numeri è:' + somma);
+//console.log('La somma dei numeri è:' + somma);
+
+
+
+// versione while
+
+// add let somma
+let somma =0;
+
+// add var i 
+var i=0
