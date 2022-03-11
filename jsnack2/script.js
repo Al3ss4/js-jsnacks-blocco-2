fed86ac4aecefd625a06ff65a5832ba15b1197a7
Mario Rovito <alessandro.r14@gmail.com>
2022-03-11 14:23:40 +0100

    fix code, for version first. add for and prompt

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,10 +3,19 @@ console.log('JS OK');
 // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
 
-// versione while
+// versione for
 
 //creazione del let
 let somma = 0;
 
 
+//creazione del for 
+for( let i=0; i < 5; i++){
+    let nuovoNumero;
+    
 
+// prompt con parseInt per dichiarare un numero 
+        nuovoNumero = parseInt(prompt(`${i+1} Inserisci un numero`))
+    
+
+}
