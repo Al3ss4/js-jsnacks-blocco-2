2653c96be17e1791e5a1ec912d644f34c94092e6
Mario Rovito <alessandro.r14@gmail.com>
2022-03-11 15:05:40 +0100

    add arrayLungo, arrayCorto e arraycasuale

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,17 @@
 console.log('JS OK');
+
+
+// Crea due array che hanno un numero di elementi diversi. Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.
+
+
+// add array più lungo
+var arrayLungo = [1,2,3,4,5,6,7,8,9];
+console.log(arrayLungo);
+// add array più corto
+var arrayCorto =[10,11,12,13];
+console.log(arrayCorto);
+
+// add array con numeri casuali 
+var arrayCasuali = [14,15,16,17,18,19,20,21];
+console.log(arrayCasuali);
+
