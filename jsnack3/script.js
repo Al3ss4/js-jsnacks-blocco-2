04a0fcbd70a0a58a6bc1ce2435bba255354d0954
Mario Rovito <alessandro.r14@gmail.com>
2022-03-14 09:25:56 +0100

    fix code

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -12,13 +12,11 @@ console.log(arrayLungo);
 var arrayCorto =[10,11,12,13];
 console.log(arrayCorto);
 
-// add array con numeri casuali 
-var arrayCasuali = [14,15,16,17,18,19,20,21];
-console.log(arrayCasuali);
+
 
 // creazione while: fintanto che gli elementi dell'array più lungo sono maggiori degli elementi dell'array più corto, allora in maniera randomica verranno aggiunti dei numeri presi da un terzo array.
 while(arrayLungo.length > arrayCorto.length) {
-    arrayCorto.push(arrayCasuali[Math.floor(Math.random()*arrayCasuali.length)])
+    arrayCorto.push(Math.floor(Math.random() * 100));
   }
   
   console.log(arrayLungo);
